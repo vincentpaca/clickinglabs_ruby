f4e1a6453e0ece384204b3b7252642b18499ee07
Vincent Paca <vincent.paca@gmail.com>
2012-08-03 14:21:51 +0800

    Added more examples

--- chapter_01/code/methods.rb
+++ chapter_01/code/methods.rb
@@ -1,6 +1,16 @@
-def foo(param1, param2)
+#this method adds the two numbers in the param
+def add(param1, param2)
   puts param1 + param2
 end
 
-foo(1,2) # You can call methods like so
-foo 1, 2 # It's okay to not include the parentheses
+add(1,2) # You can call methods like so
+add 1, 2 # It's okay to not include the parentheses
+
+#you can also have optional parameters like
+def foo(x, y=0)
+  puts x
+  puts y
+end
+
+foo 1 #=> will print out 1, then 0.
+foo 2, 3 #=> you go it.
