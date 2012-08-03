4a443899d642679cb5d7661e41716e0c98fa2a1c
Vincent Paca <vincent.paca@gmail.com>
2012-08-03 17:10:29 +0800

    Added sample code for objects

---
+++ chapter_01/code/objects.rb
@@ -0,0 +1,9 @@
+puts "a".class
+puts 1.class
+puts [1,2].class
+
+puts (1 + 2) # 1 + 2 means that 2 is passed as a parameter to 1's "+" method
+
+# << modifies the array to add new objects to the array
+array = [1,2]
+array << ["x",:y, 3]
