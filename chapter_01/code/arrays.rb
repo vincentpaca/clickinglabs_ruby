57b500d0e960988360f29cb8442ba9b75fd834f2
Vincent Paca <vincent.paca@gmail.com>
2012-08-03 14:34:30 +0800

    Added more examples to arrays

--- chapter_01/code/arrays.rb
+++ chapter_01/code/arrays.rb
@@ -1,9 +1,13 @@
+#anything goes inside arrays, numbers, strings, symbols. Anything
 array = [1, "two", :three]
 
+#we can use the 'each' method to iterate the values inside an array.
 array.each do |a|
   puts a
 end
 
-numbers = [1, 2, 3]
-
-numbers.each { |number| puts "#{number * 2}" }
+#this code simply multiplies each number inside the array
+numbers = [1, 2, 3] #lets initialize the numbers
+new_number_array = [] #initialize and empty array to store the new numbers to
+numbers.each { |number| new_number_array << (number * 2) } #notice that instead of each-do, we used a block of code to execute.
+puts new_number_array #=> shows us the numbers multiplied by 2
