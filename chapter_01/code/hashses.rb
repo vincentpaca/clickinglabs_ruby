3ecd0aac3160f9e456a5cbd9beb1f8318c8df643
Vincent Paca <vincent.paca@gmail.com>
2012-08-03 14:43:58 +0800

    Added more examples to hashes

--- chapter_01/code/hashses.rb
+++ chapter_01/code/hashses.rb
@@ -1,4 +1,11 @@
+#create a hash
 hash = { :but => "Here's my number", "so" => "Call me maybe" }
 
+#to access the values for each key, we can do:
 puts hash[:but]
 puts hash["so"]
+
+#we can also use each to iterate hashes
+hash.each do |key, value|
+  puts "#{key} -> #{value}"
+end
